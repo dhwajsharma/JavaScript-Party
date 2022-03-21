be445ad57f0418b371cd900a8d956e52ef8305c8
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-03-21 10:40:47 +0530

    Update polyfill_map.js

--- polyfill_map.js
+++ polyfill_map.js
@@ -1,13 +1,17 @@
 // Current element, its index, and the context should be passed as an argument to the callback function.
 
-Array.prototype.map = function (callback) {
-  const result = [];
-  for (let i = 0; i < this.length; i++) {
-    // cross check if the element is part of the current
-    if (this.indexOf(this[i]) > -1) {
-      result[i] = callback(this[i], i, this);
-    }
-  }
+Array.prototype.myMap = function(callback){
+	let temp = [];
+	for(let i = 0; i < this.length; i++){
+		temp.push(callback(this[i], i, this));
+	}
+	return temp;
+}
 
-  return result;
-};
+const nums = [1,2,3,4];
+
+const output = nums.myMap((num, i, arr) => {
+	return num*2;
+})
+
+console.log(output)
