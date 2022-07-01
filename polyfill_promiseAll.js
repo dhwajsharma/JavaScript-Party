a68d313049ef9f9a7d39deb998c7b7c0a509671b
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-07-01 17:15:31 +0530

    Update polyfill_promiseAll.js

--- polyfill_promiseAll.js
+++ polyfill_promiseAll.js
@@ -1,48 +1,20 @@
-function myPromiseAll(taskList) {
-  //to store results 
-  const results = [];
-
-  //to track how many promises have completed
-  let promisesCompleted = 0;
-
-  // return new promise
-  return new Promise((resolve, reject) => {
-
-    taskList.forEach((promise, index) => {
-      //if promise passes
-      promise.then((val) => {
-          //store its outcome and increment the count 
-          results[index] = val;
-          promisesCompleted += 1;
-
-          //if all the promises are completed, 
-          //resolve and return the result
-          if (promisesCompleted === taskList.length) {
-            resolve(results)
-          }
-        })
-        //if any promise fails, reject.
-        .catch(error => {
-          reject(error)
-        })
-    })
-  });
+function promiseAll(promises){
+	const result = [];
+	let counter = 0;
+	return new Promise((resolve, reject) => {
+		promises.forEach((p, index) => {
+			p.then((res) => {
+				result.push(res);
+				counter++;
+				if(counter === result.length){
+					return resolve(result);
+				}
+			})
+			.catch(err => reject(err))
+		})
+	})
 }
 
-
-function task(time) {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      resolve(time);
-    }, time);
-  });
-}
-
-const taskList = [task(1000), task(5000), task(3000)];
-
-//run promise.all
-myPromiseAll(taskList)
-  .then(results => {
-    console.log("got results", results)
-  })
-  .catch(console.error)
+promiseAll([Promise.resolve("hello"), Promise.resolve("Dhwaj")])
+.then(res => console.log("result", res))
+.catch(err => console.err("err ", err))
