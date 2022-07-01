355bc0c74af83087e9d6eeff4ede5f2d62b3476c
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-07-01 16:58:16 +0530

    Update polyfill_promiseAll.js

--- polyfill_promiseAll.js
+++ polyfill_promiseAll.js
@@ -28,3 +28,21 @@ function myPromiseAll(taskList) {
     })
   });
 }
+
+
+function task(time) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(time);
+    }, time);
+  });
+}
+
+const taskList = [task(1000), task(5000), task(3000)];
+
+//run promise.all
+myPromiseAll(taskList)
+  .then(results => {
+    console.log("got results", results)
+  })
+  .catch(console.error)
