3fd39c8f16cd44032a44902cf69c1313d29f748c
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-07-11 20:58:24 +0530

    Update polyfill_filter.js

--- polyfill_filter.js
+++ polyfill_filter.js
@@ -1,15 +1,19 @@
 // Current element, its index, and the context should be passed as an argument to the callback function.
 
-Array.prototype.myFilter = function(callback){
-	let temp = [];
+let ages = [1, 2, 3, 4, 5, 6, 7];
+
+Array.prototype.myFilter = function(callback, context){
+	let finalArray = [];
 	for(let i = 0; i < this.length; i++){
-		if(callback(this[i], i, this)) temp.push(this[i]);
+		if(callback.call(context, this[i], i, this)){
+			finalArray.push(this[i])
+		}
 	}
-	return temp;
+	return finalArray;
 }
 
-const nums = [1,2,3,4];
+let result = ages.myFilter(x => {
+	return x < 5
+})
 
-const output = nums.myFilter(num =>  num > 2)
-
-console.log(output)
+console.log(result)
